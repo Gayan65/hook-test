04a729f376ad0c3c9c2cd15e20e02a2d69064cee
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-07-03 22:36:46 +0300

    reduce obj add

--- src/hooks/personalHooks.js
+++ src/hooks/personalHooks.js
@@ -1,7 +1,9 @@
 export const reducer = (state, action) => {
   switch (action.type) {
     case "set_name":
-      return action.payload;
+      return { ...state, name: action.payload };
+    case "set_age":
+      return { ...state, age: action.payload };
     default:
       return state;
   }
--- src/pages/PersonalInfo.js
+++ src/pages/PersonalInfo.js
@@ -2,7 +2,7 @@ import { useReducer } from "react";
 import { reducer } from "../hooks/personalHooks";
 
 const PersonalInfo = () => {
-  const [state, dispatch] = useReducer(reducer, "");
+  const [state, dispatch] = useReducer(reducer, { name: "", age: "" });
 
   const handdleClick = () => {
     console.log(state);
@@ -14,9 +14,17 @@ const PersonalInfo = () => {
       <div className="personal-info-form">
         <input
           name="first_name"
-          value={state}
+          value={state.name}
           onChange={(e) =>
             dispatch({ type: "set_name", payload: e.target.value })
+          }
+        />
+        <input
+          type="number"
+          name="age"
+          value={state.age}
+          onChange={(e) =>
+            dispatch({ type: "set_age", payload: e.target.value })
           }
         />
         <button onClick={handdleClick}>Save</button>
