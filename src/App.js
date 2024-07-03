33ee22bcc2d2b3b7b2b2f3f540389956254efe16
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-07-03 21:31:09 +0300

    useSate add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PersonalInfo from "./pages/PersonalInfo";
+
 function App() {
-    return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PersonalInfo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/PersonalInfo.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const PersonalInfo = () => {
+  const [name, setName] = useState("");
+
+  const handdleClick = () => {
+    console.log(name);
+  };
+
+  return (
+    <div>
+      <h3>Personal Information</h3>
+      <div className="personal-info-form">
+        <input
+          name="first_name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={handdleClick}>Save</button>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
